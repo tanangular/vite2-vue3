38a04f3250a2e9ed19f41c65b833c4b96d4e9ac3
tanangular <tanangular@gmail.com>
2021-01-31 19:22:19 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <HelloWorld msg="Hello Vue 3 + Vite" />
+  <Timer />
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import Timer from './components/Timer.vue'
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Timer.vue
@@ -0,0 +1,29 @@
+<template>
+	Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+	Architecto fuga sunt tempora veniam voluptatem. Ab, alias aliquam
+	aliquid dolorem doloribus dolorum, inventore minus necessitatibus nesciunt
+	reiciendis sed ullam voluptatem voluptatum!
+	
+	{{ timer }}
+	
+</template>
+
+<script>
+export default {
+	name: 'Timer',
+	data() {
+		return {
+			timer: 0
+		}
+	},
+	mounted() {
+		setInterval(() => {
+			this.timer++
+		}, 1000)
+	}
+}
+</script>
+
+<style scoped>
+
+</style>
